83869229f0ec31c537529a5fea7d826817a54405
mohaned mashaly <mohaned_boss@outlook.com>
2022-01-14 15:40:01 +0200

    chore: Add the app SQL schema

---
+++ flaskr/schema.sql
@@ -0,0 +1,9 @@
+create table account(username varchar(15) 
+primary key, name varchar(20), email text);
+
+create table tweet(id serial primary key, username varchar(15),
+description text, FOREIGN KEY(username) references account(username));
+
+create table comment(id serial primary key, tweet_id int, content text, parent_commend_id int,
+FOREIGN KEY(tweet_id) references tweet(id), FOREIGN KEY(parent_commend_id) references comment(id));
+
