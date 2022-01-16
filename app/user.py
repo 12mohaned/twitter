3fdb536bb64daf2e78bee05b5d373ebe905addef
mohaned mashaly <mohaned_boss@outlook.com>
2022-01-16 13:39:06 +0200

    Feature: Add API's that support the below function post_comment(POST): API that post user comment on a tweet post_sub_comment(POST): API that post user sub-comment on a comment get_sub_comments(GET): API that fetch sub-comments on a comment get_tweet_comments(GET): API that fetch comments on a tweet

--- app/user.py
+++ app/user.py
@@ -9,14 +9,48 @@ comment_model = CommentModel()
 comment_controller = CommentController()
 user_blueprint = Blueprint('user_blueprint', __name__)
 
-@user_blueprint.route('/',methods = ["GET"])
+#below variable act as Session object
+Session = {'user_name' : 'user'}
+username = Session['user_name']
+
+@user_blueprint.route('/home',methods = ["GET"])
 def home():
     tweets = tweet_model.get_tweets()
     return jsonify(tweets)
 
-@user_blueprint.route('/',methods = ["POST"])
+@user_blueprint.route('/home/<tweet_id>',methods = ["GET"])
+def get_tweet_comments(tweet_id):  
+    """
+    This API returns the main comments on tweet.
+    """
+    response = comment_model.get_tweet_comments(tweet_id)
+    return jsonify(response)
+
+@user_blueprint.route('/home/<tweet_id>/<comment_id>',methods = ["GET"])
+def get_sub_comments(tweet_id, comment_id):  
+    """
+    This API returns the sub comments on a comment.
+    """
+    response = ""
+    if not(tweet_model.is_exists_tweet(tweet_id) is None):
+        response = comment_model.get_sub_comments(comment_id)
+    return jsonify(response)
+
+@user_blueprint.route('/home',methods = ["POST"])
 def post_tweet():
     content = request.form.get("content")
     if(tweet_controller.is_tweet_valid(content)):
-        tweet_model.add_tweet("", content,datetime.datetime.now)
+        tweet_model.add_tweet(username, content,datetime.datetime.now)
 
+
+@user_blueprint.route('/home/<tweet_id>',methods = ["POST"])
+def post_comment(tweet_id):
+    content = request.form.get("content")
+    if not comment_controller.is_comment_empty(content):
+        comment_model.add_comment(tweet_id, content, None)
+
+@user_blueprint.route('/home/<tweet_id>/<parent_comment_id>',methods = ["POST"])
+def post_sub_comment(tweet_id,parent_comment_id):
+    content = request.form.get("content")
+    if not comment_controller.is_comment_empty(content):
+        comment_model.add_comment(tweet_id,content,parent_comment_id)
