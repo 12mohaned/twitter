983ca6d2317123fc08c398965f90d294cb5f22c6
mohaned mashaly <mohaned_boss@outlook.com>
2022-01-17 21:15:22 +0200

    refactor: Edit comment table to contain username of the user that commented

--- app/schema.sql
+++ app/schema.sql
@@ -4,5 +4,6 @@ primary key, name varchar(20), email text);
 create table tweet(id serial primary key, username varchar(15), time_posted timestamp,
 description text, FOREIGN KEY(username) references account(username));
 
-create table comment(id serial primary key, tweet_id int, content text, parent_commend_id int,
-FOREIGN KEY(tweet_id) references tweet(id), FOREIGN KEY(parent_commend_id) references comment(id));
+create table comment(id serial primary key, tweet_id int, content text, parent_commend_id int, username varchar(15),
+FOREIGN KEY(tweet_id) references tweet(id), FOREIGN KEY(parent_commend_id) references comment(id),
+FOREIGN KEY(username) references account(username));
