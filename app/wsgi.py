bde4884e46308fdbea7b19b16313d40789579a80
mohaned mashaly <mohaned_boss@outlook.com>
2022-01-17 16:00:48 +0200

    style: rename app.py

---
+++ app/wsgi.py
@@ -0,0 +1,9 @@
+"""
+The end point of the platform that run the app, and
+it register other blueprints(components), i.e: user component.
+"""
+
+from flask import Flask
+from user import user_blueprint
+app = Flask(__name__)
+app.register_blueprint(user_blueprint)
