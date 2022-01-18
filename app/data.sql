a1b4c2d5c5837d0f20623783b1e3f3e98a9e2fff
mohaned mashaly <mohaned_boss@outlook.com>
2022-01-18 12:08:26 +0200

    docs: Add script that generate data

---
+++ app/data.sql
@@ -0,0 +1,6 @@
+INSERT into account(username)VALUES('mohaned');
+
+INSERT into tweet(username, description)VALUES('mohaned','Hello Twitter'); 
+
+INSERT into comment(tweet_id, content, username)VALUES(1,'Hello mohaned','mohaned'); 
+
